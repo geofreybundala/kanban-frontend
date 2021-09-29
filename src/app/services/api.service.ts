4647deb716f6b2e76e01affe268955b0c2d3fea9
geofreybundala <bundalageofrey2@gmail.com>
2021-09-29 22:03:27 +0300

    fix(services  folder)

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,6 @@ export class ApiService {
 private BASE_URL = 'http://localhost:3000/';
   constructor(private httpClient: HttpClient) { }
 
-
   public get(resource: string){
     return this.httpClient.get(this.BASE_URL+resource);
   }
