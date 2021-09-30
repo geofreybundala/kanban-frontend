5e33cd90e0f6bfd3069c612ec77901d34ac31f67
geofreybundala <bundalageofrey2@gmail.com>
2021-09-30 20:53:28 +0300

    🎨 FIX: improve folder structures

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,8 +8,20 @@ export class ApiService {
 private BASE_URL = 'http://localhost:3000/';
   constructor(private httpClient: HttpClient) { }
 
-  public get(resource: string){
+  public findAll(resource: string){
     return this.httpClient.get(this.BASE_URL+resource);
+  }
+
+  public findOne(resource:string,id: string){
+    return this.httpClient.get(`${this.BASE_URL}${resource}/${id}`);
+  }
+
+    // public update(resource:string,id: string){
+  //   return this.httpClient.patch(`${this.BASE_URL}${resource}/${id}`);
+  // }
+
+  public deleteOne(resource:string,id: string){
+    return this.httpClient.delete(`${this.BASE_URL}${resource}/${id}`);
   }
 
 }
